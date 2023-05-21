02d116bed5c5c4caafba039a584dc0113cb0f0ea
AbsolucionArtistica <122127365+AbsolucionArtistica@users.noreply.github.com>
2023-05-21 19:39:29 -0400

    actualizacion4

--- src/app.js
+++ src/app.js
@@ -5,28 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+let suit = ["♦", "♥", "♠", "♣"];
+let number = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+  "13"
+];
+
+function CardGenerator() {
   //write your code here
-  let suit = ["♦", "♥", "♠", "♣"];
-  let number = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
+
   let randomSuit = suit[Math.floor(Math.random() * suit.length)]; //Elige una pinta al azar
   let randomNumber = number[Math.floor(Math.random() * number.length)]; //Elige un numero al alzar
 
-  const card1 = document.querySelector(".card1");
+  const originalCards = document.querySelector(".originalCards");
+  const card1 = document.createElement("div");
+  card1.classList.add("card1");
   const myDiv = document.createElement("div");
 
   if (randomSuit == "♥") {
@@ -47,13 +51,39 @@ window.onload = function() {
   Number.classList.add("number"); //Aplica la clase para la pinta de carta que salio
   LowerSuit.classList.add("suit", "rotate"); //Aplica la clase para el numero de carta que salio
 
+  if (randomNumber == "1") {
+    //aplica la pinta aleatorio al texto
+    Number.innerHTML = "A";
+  } else if (randomNumber == "11") {
+    Number.innerHTML = "J";
+  } else if (randomNumber == "12") {
+    Number.innerHTML = "Q";
+  } else if (randomNumber == "13") {
+    Number.innerHTML = "K";
+  } else Number.innerHTML = randomNumber;
+
   UpperSuit.innerHTML = randomSuit; //aplica el numero aleatorio al texto
-  Number.innerHTML = randomNumber; //aplica la pinta aleatorio al texto
   LowerSuit.innerHTML = randomSuit; //aplica el numero aleatorio al texto
 
+  originalCards.appendChild(card1);
   card1.appendChild(myDiv);
   myDiv.appendChild(UpperSuit);
   myDiv.appendChild(Number);
   myDiv.appendChild(LowerSuit);
   console.log(randomSuit);
-};
+
+  return myDiv;
+}
+
+const numberOfCards = document.getElementById("numberInput");
+const drawButton = document.getElementById("drawButton");
+let generatedCards = [];
+
+drawButton.addEventListener("click", function() {
+  const cantidad = parseInt(numberOfCards.value);
+
+  for (let i = 0; i < cantidad; i++) {
+    let card = CardGenerator();
+    generatedCards.push(card);
+  }
+});
